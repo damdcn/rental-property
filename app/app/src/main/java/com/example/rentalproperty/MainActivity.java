69eec4c909d0c21cda69a88043740e82197bc706
Damien <damiend34@hotmail.fr>
2021-03-31 16:56:10 +0200

    basic homepage and fragment

--- app/app/src/main/java/com/example/rentalproperty/MainActivity.java
+++ app/app/src/main/java/com/example/rentalproperty/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.rentalproperty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment;
+                FragmentManager fm;
+                FragmentTransaction ft;
+
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        setFragment(new HomeFragment());
+                        break;
+                    case R.id.action_upload:
+                        setFragment(new UploadFragment());
+                        break;
+                    case R.id.action_account:
+                        setFragment(new LoginFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+            .replace(R.id.mainFragment, fragment)
+            .commit();
     }
 }
